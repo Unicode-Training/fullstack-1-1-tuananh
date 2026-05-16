fa912ec60c357d48445a47968d619d49f92fb943
Hoang An Unicode <hoangan.web@gmail.com>
2026-05-16 22:41:48 +0700

    Add delete functionality for product items in the product list

--- 4.hocjs/4.6.dom/js/ex04.js
+++ 4.hocjs/4.6.dom/js/ex04.js
@@ -27,8 +27,28 @@ const renderProducts = () => {
         return `<div class="product-item">
         <h2>${product.name}</h2>
         <button>Sửa</button>
-        <button>Xóa</button>
+        <button class="delete-btn" data-id="${product.id}">Xóa</button>
       </div>`
     }).join('')
 }
+const addEventDelete = () => {
+    const productListEl = document.querySelector('.product-list');
+    productListEl.addEventListener('click', (e) => {
+        if (e.target.classList.contains('delete-btn')) {
+            const id = +e.target.dataset.id;
+            const index = products.findIndex((product) => product.id === id);
+            products.splice(index, 1);
+            renderProducts();
+        }
+    });
+}
 renderProducts();
+addEventDelete();
+
+
+
+//Chức năng xóa
+//B1: Lắng nghe event click vào nút xóa
+//B2: Lấy productId tương ứng
+//B3: Xóa phần tử trong mảng products tương ứng với id
+//B4: Gọi lại hàm renderProducts
