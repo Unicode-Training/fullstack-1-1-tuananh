126b853c40d482226097cea585e3f79c9330d601
Hoang An Unicode <hoangan.web@gmail.com>
2026-06-14 08:36:01 +0700

    Add product search and pagination functionality with modal support

--- 4.hocjs/4.8.fetch-api/js/ex01.js
+++ 4.hocjs/4.8.fetch-api/js/ex01.js
@@ -31,19 +31,37 @@
 
 // }
 // addProduct();
-
+const LIMIT = 9;
+let currentPage = 1;
+let keyword = "";
 const getProducts = async () => {
     try {
-        const response = await fetch(`https://dummyjson.com/products`);
+        const skip = (currentPage - 1) * LIMIT;
+        const response = await fetch(`https://dummyjson.com/products/search?q=${keyword}&limit=${LIMIT}&skip=${skip}`);
         if (!response.ok) {
             throw new Error("Failed to fetch products");
         }
-        const { products } = await response.json();
+        const { products, total } = await response.json();
+        const pageNumber = Math.ceil(total / LIMIT);
+        renderPagination(pageNumber);
         renderProducts(products);
     } catch (error) {
         console.log(error.message);
 
     }
+}
+
+const renderPagination = (pageNumber) => {
+    const paginationEl = document.querySelector('.js-pagination');
+    let html = '';
+    for (let i = 1; i <= pageNumber; i++) {
+        html += `<button
+          class="px-3 py-1 bg-green-600 text-white hover:bg-red-600 cursor-pointer ${i === currentPage ? 'bg-red-600' : ''}"
+        >
+          ${i}
+        </button>`;
+    }
+    paginationEl.innerHTML = html;
 }
 
 const renderProducts = (products) => {
@@ -62,26 +80,120 @@ const renderProducts = (products) => {
     productListEl.innerHTML = html
 }
 
-const addEvent = () => {
+const addEventButton = () => {
     const productListEl = document.querySelector('.js-product-list');
     productListEl.addEventListener('click', (e) => {
         if (e.target.classList.contains('js-detail-btn')) {
             const id = e.target.dataset.id;
+            openModal();
             getProduct(id);
         }
-    })
+    });
 }
-
+const renderProductDetailModal = (data) => {
+    const modalContent = document.querySelector('.js-modal-content');
+    modalContent.innerHTML = `<h2 class="text-xl mb-2">${data.title}</h2>
+        <p class="mb-2">Giá: ${data.price}</p>
+        <img
+          src="${data.thumbnail}"
+          alt=""
+        />
+        <div class="flex gap-2 mt-3">
+          ${data.images.map(item => `<div class="w-[100px]">
+            <img
+              src="${item}"
+              alt=""
+            />
+          </div>`).join('')}
+          
+        </div>
+        <p class="mt-3">
+         ${data.description}
+        </p>`
+}
 const getProduct = async (id) => {
     try {
         const response = await fetch(`https://dummyjson.com/products/${id}`);
         const data = await response.json();
-        console.log(data);
+        renderProductDetailModal(data);
     } catch (error) {
         console.log(error);
 
     }
 }
 
-addEvent();
+//closure
+const debounce = (fn, timeout = 500) => {
+    let id;
+    return (...args) => {
+        if (id) {
+            clearTimeout(id);
+        }
+        id = setTimeout(() => {
+            fn(...args);
+        }, timeout)
+    }
+}
+
+const addEventSearch = () => {
+    const searchEl = document.querySelector('.js-search');
+    searchEl.addEventListener('input', debounce(e => {
+        keyword = e.target.value;
+        getProducts();
+    }, 1000))
+}
+const addEventPagination = () => {
+    const paginationEl = document.querySelector('.js-pagination');
+    paginationEl.addEventListener('click', (e) => {
+        const page = +e.target.innerText;
+        currentPage = page;
+        getProducts();
+    })
+}
+const openModal = () => {
+    const modalEl = document.querySelector('.js-modal');
+    modalEl.classList.remove('hidden');
+}
+const closeModal = () => {
+    const modalEl = document.querySelector('.js-modal');
+    modalEl.classList.add('hidden');
+    const modalContent = document.querySelector('.js-modal-content');
+    modalContent.innerHTML = `<span class="text-2xl text-center block">Loading...</span>`;
+}
+const addEventModal = () => {
+    const modalOverlay = document.querySelector('.js-modal-overlay');
+    modalOverlay.addEventListener('click', closeModal);
+}
+const addEventNewItem = () => {
+    const addBtn = document.querySelector('.js-add-btn');
+    addBtn.addEventListener('click', () => {
+        openModal();
+        renderAddProductForm();
+    })
+}
+const renderAddProductForm = () => {
+    const modalContent = document.querySelector('.js-modal-content');
+    modalContent.innerHTML = `
+    <form>
+        <div class="mb-3">
+            <label>Name</label>
+            <input class="px-3 py-1 outline-none border border-[#ddd] w-full" placeholder="Name..." />
+        </div>
+    </form>
+    `
+}
+addEventNewItem();
+addEventModal();
+addEventPagination();
+addEventSearch();
+addEventButton();
 getProducts();
+
+
+
+//Kỹ thuật debounce: Dựa vào tốc độ thực thi (500ms)
+//Ví dụ: keyword = abc
+//a --> id1 = setTimeout(callback, 500ms)
+//ab --> xóa id1, id2 = setTimeout(callback, 500ms)
+//abc --> xóa id2, id3 = setTimeout(callback, 500ms);
+//setTimeout chưa kịp chạy -> Đã bị xóa
